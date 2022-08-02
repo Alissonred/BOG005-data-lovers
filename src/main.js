f9b8f264f80d71b3517570cf3e4e8cc87ebdbb49
Camilaf19 <cam.fonseca19@gmail.com>
2022-08-02 12:05:39 -0500

    Version 1 visualiición de todo menos objetos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+var characteresSerie = document.getElementById("personajes");
+
+for(let personajes of data.results) {
+    console.log(personajes);
+    characteresSerie.innerHTML += `<article> 
+    <h2>${personajes.name}</h2>
+    <br>
+    <img src=${personajes.image} alt="muestra" >
+    <p>${personajes.status}</p>
+    <br>
+    <p2>${personajes.species}</p2>
+    <br>
+    <p3>${personajes.type}</p3>
+    <br>
+    <p4>${personajes.gender}</p4>
+    </article>`;
+}
+
+
