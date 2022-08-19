520624a18cf7aa1c740907fbc0588ba4636ad11f
Alissonred <108753902+Alissonred@users.noreply.github.com>
2022-08-19 11:52:51 -0500

    calcular casi completa, falta css

--- src/main.js
+++ src/main.js
@@ -43,68 +43,74 @@ const buttonAll = document.getElementById("buttonAll");
 buttonAll.addEventListener("click", () => showData(data.results));
 
 /////////////////////////// calcular/////////////////////////////////////
-let score = 0; 
+let score = 0;
 let countQuestions = 0;
 const buttonGame = document.getElementById("buttonGame");
 buttonGame.addEventListener("click", () => showGame(data.results));
 
 let labelask_pre = ["name", "origin name", "species", "status", "location name"]; // pregunda directa escrita para user
-let respuestas_pre = ["name", "image"]; // seleccion de referencia
 let preguntas_pre = ["name", "origin.name", "species", "status", "location.name"]; // pregunda directa para evaluar
-let opciones = [];
-
-/* let propAsk =["name", "image"];
-let propRta =["name", "origin.name", "species", "status", "location.name"];
-let labelAsk = []; */
-/* let propAsk ="";
-let propRta ="";
-if(){ propRta = "name"; propAsk = "image"; } // opciones de nombre
-else if(){ propRta = "origin.name"; propAsk = "name"; } // opciones de origen
-else if(){ propRta = "species"; propAsk = "name";} // opciones de especie
-else if(){ propRta = "status"; propAsk = "image";} // opciones de estado
-else if(){ propRta = "location.name"; propAsk = "image"; } // opciones de ubicacion */
-
 
 function showGame(characters) {
   countQuestions++; ///contador de preguntas
-  let item_aleat= Math.floor(Math.random() * (1 + 4) + 0); //// indice aleatorio
+  let item_aleat = Math.floor(Math.random() * (1 + 4) + 0); //// indice aleatorio para propiedades a preguntar
+  let item_aleat1 = Math.floor(Math.random() * (1 + 1) + 0); //// indice aleatorio para selec personaje a preguntar
   let labelask = labelask_pre[item_aleat]; /// pregunta aleatoria
- // seleccion de opciones
-  let answerRandom = [characters[Math.floor(Math.random() * (1 + 493) + 0)], characters[Math.floor(Math.random() * (1 + 493) + 0)]];
-  let answerOk =  answerRandom[Math.floor(Math.random()*(1+2) + 0)];// elemento a evaluar (aleatorio de lista de opc)
-  console.log(answerRandom); // opciones
+  let answerRandom = [characters[Math.floor(Math.random() * (1 + 493) + 0)], characters[Math.floor(Math.random() * (1 + 493) + 0)]];//opciones
+  let answerOk = answerRandom[item_aleat1];//selecc una de las 2 opc como correcta
   console.log(labelask + " es lo que pregunta");// que está preguntando
-  //console.log(answerOk.name + " es a quien pregunta");// a quien pregunta
-  console.log(answerOk[preguntas_pre[item_aleat]]  + " es la respuesta" );// cual es la rta
- 
+  console.log(answerOk.name + " es a quien pregunta");// a quien pregunta
+  console.log(answerOk[preguntas_pre[item_aleat]] + " es la respuesta");// cual es la rta
+
   charactersSerie.innerHTML = "";
   charactersSerie.innerHTML = `
-  <article id=Gamecontainer> <h4> what is the ${labelask} to character showed?</h4>
+  <article id=Gamecontainer> <h4> The ${labelask} to character showed is ${answerRandom[preguntas_pre[item_aleat]]} ?</h4>
   <img src=${answerOk.image} alt="${answerOk.name}">
     <br>
   <select name="select an answer" id="opc_img">
   <option disabled selected ="Select an answer"></option>
-  <option id="answer1" value="${answerRandom[0][preguntas_pre[item_aleat]]}"> ${answerRandom[0][preguntas_pre[item_aleat]]}</option>
-  <option id="answer2" value="${answerRandom[1][preguntas_pre[item_aleat]]}">${answerRandom[1][preguntas_pre[item_aleat]]}</option>
+  <option id="answer1" value="true"> True</option>
+  <option id="answer2" value="false"> False</option>
   </select>
+  <p id="puntaje" >Puntaje acumulado: ${score}</p>
+
   <button id="ButtonNext">Next</button>
   </article>
   `;
 
   charactersSerie.querySelector("#ButtonNext").addEventListener("click", () => showGame(data.results))
-  charactersSerie.querySelector("#opc_img").addEventListener("change", (event) => {if (event.target.value === answerOk[preguntas_pre[item_aleat]]) { score++; }})
-  console.log(score);
+  charactersSerie.querySelector("#opc_img").addEventListener("change", (event) => {
+    if (event.target.value === "true") {
+      if (answerRandom[preguntas_pre[item_aleat]] === answerOk[preguntas_pre[item_aleat]]) { /// evalua que selecciono true y era v
+        score++;
+        alert("excelente" + " puntaje acumulado " + score);
+        console.log(score);
+      }
+      else { alert("fallaste :("); }
+    }
+    else if (event.target.value === "false") {
+      if (answerRandom[preguntas_pre[item_aleat]] !== answerOk[preguntas_pre[item_aleat]]) { // evalua que selec false y era
+        score++;
+        alert("excelente" + "puntaje acumulado" + score);
+        console.log(score);
+      }
+      else { alert("fallaste"); }
+    }
+  })
+  console.log("puntaje: " + score);
+
   if (countQuestions === 6) {
-    alert("terminaste" + "tu puntaje es" + score)
-    let category= "";
-    if (score<2){category = "principiante"}
-    else if (score>=2 && score<=3 ){category = "intermedio"}
-    else if (score>=3 && score<=5 ){category = "avanzado"}
+    alert("terminaste, tu puntaje es" + score)
+    let category = "";
+    if (score < 2) { category = " principiante" }
+    else if (score >= 2 && score <= 3) { category = " intermedio" }
+    else if (score >= 3 && score <= 5) { category = " avanzado" }
     charactersSerie.innerHTML = "";
-    charactersSerie.innerHTML =  `
-  <article id=results> you are finish, your score is: ${score}, your category is ${category}
+    charactersSerie.innerHTML = `
+  <article id= "results"> you  finished, your score is: ${score}, your category is ${category}
   </article>
   `;
   }/// Resultados de juego
-  
+
 }
+
